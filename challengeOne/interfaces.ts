4acda5584fe96114573b14ca34d8a317441b511e
Pavel Popov <pcveykos@gmail.com>
2022-10-17 14:41:43 +0200

    Add first challenge

---
+++ challengeOne/interfaces.ts
@@ -0,0 +1,39 @@
+
+import houses from './houses.json'
+
+interface House {
+  name: string
+  planets: string | string[]
+}
+
+
+interface HouseWithID extends House {
+  id:number
+}
+
+// function findHouses(houses: string): HouseWithID[];
+// function findHouses(
+//   houses: string,
+//   filter: (house: House) => boolean
+// ): HouseWithID[];
+// function findHouses(houses: House[]): HouseWithID[];
+// function findHouses(
+//   houses: House[],
+//   filter: (house: House) => boolean
+// ): HouseWithID[];
+
+function findHouses(input : string | House[], filter?: (house: House)=> boolean) : HouseWithID[] {
+  const houses: House[] = typeof input === "string" ? JSON.parse(input) : input;
+
+  return (filter ? houses.filter(filter) : houses)
+  .map(house => ({
+    id: houses.indexOf(house),
+    ...house
+  }))
+}
+
+console.log(
+  findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
+);
+
+console.log(findHouses(houses, ({ name }) => name === "Harkonnen"));
